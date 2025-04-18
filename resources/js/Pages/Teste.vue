0477c11fdd9a6790555a813d40a69d3b759b8382
iagosm <iagosousa201486@gmail.com>
2025-04-17 23:19:56 -0300

    adicionando layout da tela principal

--- resources/js/Pages/Teste.vue
+++ resources/js/Pages/Teste.vue
@@ -1,7 +1,12 @@
+<script setup>
+// Com fill-corquequero consigo definir a cor na interna do icone
+// Com color eu seto a cor dele padrão
+import {ThumbsDown, ThumbsUp, MessageSquare} from 'lucide-vue-next'
+</script>
 <template>
   <header class="flex items-center justify-between px-6 py-4 bg-white shadow-sm">
     <div class="flex items-center space-x-2">
-      <!-- <img src="/logo.svg" alt="Logo" class="w-6 h-6"> -->
+      <!-- <img src="/logo.svg" alt="Logo" class="w-4 h-6"> -->
       <span class="text-xl font-bold">Desabafaê</span>
     </div>
     <nav class="hidden space-x-6 md:flex">
@@ -16,9 +21,48 @@
       <button class="px-4 py-2 text-sm font-medium text-white rounded-full bg-lime-400 hover:bg-lime-500">Cadastrar</button>
     </div>
   </header>
-  <main>
+  <main class="w-full">
+    <div class="">
+     <h2>Reclamações</h2> 
+      <div class="grid grid-cols-1 gap-4">
+        <div class="p-4 border border-red-400 rounded-sm">
+          <div v-for="n in 10" class="flex justify-between p-3 mb-4 border border-gray-400 rounded">
+            <div>
+              <div>
+                <h2 class="text-xl font-bold">Problema com a entrega do produto</h2>
+              </div>
+              <div>Houve um problema com a e entrega do produ...</div>
+              <div>Empresa</div>
+              <!-- <ThumbsDown class="w-4 h-6 text-red-600 bg-blue-800 fill-red-700" /> <ThumbsUp class="w-4 h-6 text-red-600" />
+              <ThumbsDown class="w-4 h-6 text-red-600 bg-blue-800 fill-red-700" /> <ThumbsUp class="w-4 h-6 text-red-600" />
+              <MessageSquare class="w-4 h-6 text-red-600 bg-blue-800 fill-red-700" /> <ThumbsUp class="w-4 h-6 text-red-600" /> -->
+              <div class="mt-2 flex align-middle justify-between max-w-[150px]">
+                <div class="flex gap-1">
+                  <ThumbsUp class="w-4 h-4 text-black"/>
+                  <span>12</span>
+                </div>
+                <div class="flex gap-1">
+                  <ThumbsDown class="w-4 h-6 text-black"/>
+                  <span>52</span>
+                </div>
+                <div class="flex gap-1">
+                  <MessageSquare class="w-4 h-6 text-black"/>
+                  <span>90</span>
+                </div>
+              </div>
+            </div>
+            <div class="flex flex-col justify-between">
+              <div class="p-1 bg-gray-100 rounded">Pendente</div>
+              <div>
+                <a href="" class="p-1 rounded-md text-lime-500 hover:text-lime-400">Ver Mais</a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- ... -->
+        <div>09</div>
+      </div>
+    </div>
   </main>
 </template>
-<script>
-</script>
 <style scoped></style>
