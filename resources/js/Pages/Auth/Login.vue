0d3b0c64d921df4423157e7610531099579a0eec
iagosm <iagosousa201486@gmail.com>
2025-04-25 21:52:40 -0300

    :construction: feat: Estilizando as telas de de login, registro e ajuste na tela principal

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -38,7 +38,7 @@ const submit = () => {
           <label for="email" class="block text-sm font-medium text-gray-900">E-mail</label>
           <div class="mt-2">
             <input v-model="form.email" required autofocus id="email" name="email" type="email" autocomplete="email"
-              class="block w-full px-3 py-2 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
+              class="block w-full px-3 py-2 border-gray-300 rounded-md shadow-sm focus:border-lime-500 focus:ring-lime-500 sm:text-sm">
           </div>
           <InputError class="mt-2" :message="form.errors.email" />
         </div>
@@ -47,16 +47,16 @@ const submit = () => {
           <div class="mt-2">
             <input id="password" name="password" v-model="form.password" type="password" autocomplete="current-password"
               required
-              class="block w-full px-3 py-2 border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
+              class="block w-full px-3 py-2 border-gray-300 rounded-md shadow-sm focus:border-lime-500 focus:ring-lime-500 sm:text-sm">
             <InputError class="mt-2" :message="form.errors.password" />
           </div>
         </div>
-        <div>
-          <Link v-if="canResetPassword" :href="route('password.request')"
-            class="text-sm text-gray-600 underline rounded-md hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-          Esqueceu a senha
-          </Link>
-        </div>
+        <div class="flex items-center justify-end mt-4 mb-4">
+					<Link :href="route('password.request')"
+						class="text-sm text-gray-500 underline rounded-md hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-lime-500 focus:ring-offset-2">
+					Já Esqueceu a senha?
+					</Link>
+				</div>
         <div class="block mt-4">
           <label class="flex items-center">
             <Checkbox name="remember" v-model:checked="form.remember" />
@@ -67,17 +67,16 @@ const submit = () => {
         </div>
         <div>
           <button :class="{ 'opacity-25': form.processing }" :disabled="form.processing"
-            class="flex justify-center w-full px-3 py-2 text-sm font-semibold text-white bg-indigo-600 rounded-md shadow-sm hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+            class="flex justify-center w-full px-3 py-2 text-sm font-semibold text-white rounded-md shadow-sm bg-lime-500 hover:bg-lime-600 focus:outline-none focus:ring-2 focus:ring-lime-500 focus:ring-offset-2">
             Entrar
           </button>
         </div>
       </form>
       <p class="mt-10 text-sm text-center text-gray-500">
         Não possui conta?
-        <!-- TODO adicionar validação se pode registarar -->
         <Link :href="route('register')"
-          class="text-sm text-gray-600 underline rounded-md hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
-        Cadastre-se
+          class="text-sm text-gray-600 underline rounded-md hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-lime-500 focus:ring-offset-2">
+           Cadastre-se
         </Link>
       </p>
     </div>
--- resources/js/Pages/Teste.vue
+++ resources/js/Pages/Teste.vue
@@ -2,14 +2,21 @@
 // Com fill-corquequero consigo definir a cor na interna do icone
 // Com color eu seto a cor dele padrão
 import { ThumbsDown, ThumbsUp, MessageSquare } from "lucide-vue-next";
-import { ref } from "vue";
-import { Link, router } from "@inertiajs/vue3"
+import { ref, computed } from "vue";
+import { Link, router, usePage } from "@inertiajs/vue3"
+import Dropdown from '@/Components/Dropdown.vue';
+import DropdownLink from '@/Components/DropdownLink.vue';
 const activeTab = ref('mais-curtidas');
 
- const getComplaint = ($idComplaint)  => {
+const getComplaint = ($idComplaint) => {
   sessionStorage.setItem('complaintId', $idComplaint);
   router.visit(route('complaint'))
- };
+};
+const isLoggedIn = computed(() => page.props.auth.user)
+const page = usePage()
+const user = page.props.auth.user
+console.log(page)
+console.log(user)
 </script>
 <template>
   <header class="flex items-center justify-between px-6 py-4 shadow-sm bg-[#F8FAFC">
@@ -25,12 +32,42 @@ const activeTab = ref('mais-curtidas');
       <a href="#" class="text-gray-800 hover:text-black">Contato</a>
     </nav>
     <div class="flex items-center space-x-3">
-      <button class="px-4 py-2 text-sm border border-gray-300 rounded-lg hover:bg-gray-100">
-        Entrar
-      </button>
-      <button class="px-4 py-2 text-sm font-medium text-white rounded-lg bg-lime-500 hover:bg-lime-600">
-        Cadastrar
-      </button>
+      <div v-if="!isLoggedIn" class="flex items-center space-x-3">
+        <Link href="/login" class="px-4 py-2 text-sm border border-gray-300 rounded-lg hover:bg-gray-100">
+          Entrar
+        </Link>
+        <Link href="/register" class="px-4 py-2 text-sm font-medium text-white rounded-lg bg-lime-500 hover:bg-lime-600">
+          Cadastrar
+        </Link>
+      </div>
+      <div v-else class="relative ms-3">
+        <Dropdown align="right" width="48">
+          <template #trigger>
+            <span class="inline-flex rounded-md">
+              <button type="button"
+                class="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-gray-500 transition duration-150 ease-in-out bg-white border border-transparent rounded-md hover:text-gray-700 focus:outline-none">
+                {{ $page.props.auth.user.name }}
+
+                <svg class="-me-0.5 ms-2 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+                  fill="currentColor">
+                  <path fill-rule="evenodd"
+                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                    clip-rule="evenodd" />
+                </svg>
+              </button>
+            </span>
+          </template>
+
+          <template #content>
+            <DropdownLink :href="route('profile.edit')">
+              Perfil
+            </DropdownLink>
+            <DropdownLink :href="route('logout')" method="post" as="button">
+              Sair
+            </DropdownLink>
+          </template>
+        </Dropdown>
+      </div>
     </div>
   </header>
   <main class="max-w-screen-xl px-2 py-8 mx-auto">
@@ -70,7 +107,8 @@ const activeTab = ref('mais-curtidas');
     <div class="p-6">
       <div v-if="activeTab === 'mais-curtidas'" class="space-y-6">
         <div class="grid grid-cols-2 gap-4">
-          <div v-for="n in 10" class="flex justify-between p-5 border border-gray-200 rounded-lg shadow-sm mb-4bg-white dark:bg-gray-800 dark:border-gray-700">
+          <div v-for="n in 10"
+            class="flex justify-between p-5 border border-gray-200 rounded-lg shadow-sm mb-4bg-white dark:bg-gray-800 dark:border-gray-700">
             <div>
               <div class="space-y-2">
                 <h2 class="text-xl font-semibold text-gray-800">Problema com a entrega do produto</h2>
@@ -80,18 +118,25 @@ const activeTab = ref('mais-curtidas');
               <!-- <ThumbsDown class="w-4 h-6 text-red-600 bg-blue-800 fill-red-700" /> <ThumbsUp class="w-4 h-6 text-red-600" />
             <ThumbsDown class="w-4 h-6 text-red-600 bg-blue-800 fill-red-700" /> <ThumbsUp class="w-4 h-6 text-red-600" />
             <MessageSquare class="w-4 h-6 text-red-600 bg-blue-800 fill-red-700" /> <ThumbsUp class="w-4 h-6 text-red-600" /> -->
-            <div class="flex items-center justify-start gap-4 mt-4 text-sm text-gray-500">
-              <div class="flex items-center gap-1"><ThumbsUp class="w-4 h-4" /><span>12</span></div>
-              <div class="flex items-center gap-1"><ThumbsDown class="w-4 h-4" /><span>52</span></div>
-              <div class="flex items-center gap-1"><MessageSquare class="w-4 h-4" /><span>90</span></div>
-            </div>
+              <div class="flex items-center justify-start gap-4 mt-4 text-sm text-gray-500">
+                <div class="flex items-center gap-1">
+                  <ThumbsUp class="w-4 h-4" /><span>12</span>
+                </div>
+                <div class="flex items-center gap-1">
+                  <ThumbsDown class="w-4 h-4" /><span>52</span>
+                </div>
+                <div class="flex items-center gap-1">
+                  <MessageSquare class="w-4 h-4" /><span>90</span>
+                </div>
+              </div>
             </div>
             <div class="flex flex-col justify-between">
               <div class="px-2 py-1 text-xs font-medium bg-gray-100 rounded w-fit">
                 Pendente
               </div>
               <div>
-                 <Link @click.prevent="getComplaint()" class="p-1 rounded-md text-lime-500 hover:text-lime-600">Ver Mais</Link>
+                <Link @click.prevent="getComplaint()" class="p-1 rounded-md text-lime-500 hover:text-lime-600">Ver Mais
+                </Link>
               </div>
             </div>
           </div>
@@ -101,7 +146,10 @@ const activeTab = ref('mais-curtidas');
   </main>
   <footer class="py-4 border-t">
     <div class="text-center">
-      <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a href="https://www.linkedin.com/in/iagosm/" target="_blank" class="hover:underline text-lime-600">Iago Sousa</a>. Todos os Direitos Reservados.</span>
+      <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a
+          href="https://www.linkedin.com/in/iagosm/" target="_blank" class="hover:underline text-lime-600">Iago
+          Sousa</a>.
+        Todos os Direitos Reservados.</span>
     </div>
   </footer>
 </template>
